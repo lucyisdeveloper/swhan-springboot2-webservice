2c4a175e88cf745716357abeb4939191224dcc4b
lucy <starwing9997@gmail.com>
2022-09-18 16:45:10 +0900

    롬복을 이용한 간단한 테스트 코드 추가

---
+++ src/main/java/com/swhan/project/Application.java
@@ -0,0 +1,16 @@
+package com.swhan.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+* NAME : Application
+* DESC : 메인클래스
+* */
+@SpringBootApplication //스프링 부트의 자동 설정, 스프링 Bean 읽기/생성 모두 자동 가능 --> 반드시 최상단에 위치해야함
+public class Application {
+    public static void main(String[] args){
+        //SpringApplication.run() : main내에서 실행 시, 내장 WAS를 실행!
+        SpringApplication.run(Application.class, args);
+    }
+}
